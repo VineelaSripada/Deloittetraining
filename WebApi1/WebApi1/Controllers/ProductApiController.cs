f46c738aeaacb0f4e98ab2a9a74f51505060a412
VineelaSripada <129578909+VineelaSripada@users.noreply.github.com>
2024-09-10 18:29:20 +0530

    Add files via upload

---
+++ WebApi1/WebApi1/Controllers/ProductApiController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static WebApi1.Models.EntityModels;
+
+namespace WebApi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+       
+       
+        public ProductDbContext _context;
+
+        public ProductApiController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            List<Product> products = _context.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            Product obj = _context.Products.Find(id);
+
+            if (obj != null)
+            {
+                return Ok(obj);
+            }
+            else
+            {
+                return NotFound(new { status = "product details donot exists." });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Product obj)
+        {
+            _context.Products.Add(obj);
+            _context.SaveChanges();
+            return Ok(new { status = "New product details added." });
+        }
+
+        [HttpPut]
+        public IActionResult EditStudent(Product obj)
+        {
+            _context.Products.Update(obj);
+            _context.SaveChanges();
+            return Ok(new { status = "Product details updated in server." });
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudentById(int id)
+        {
+            Product obj = _context.Products.Find(id);
+
+            if (obj != null)
+            {
+                _context.Products.Remove(obj);
+                _context.SaveChanges();
+                return Ok(new { status = "Product details are deleted from server." });
+            }
+            else
+            {
+                return NotFound(new { status = "Requested student details does not exists." });
+            }
+        }
+
+    }
+}
---
+++ WebApi1/WebApi1/Models/EntityModels.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi1.Models
+{
+    public class EntityModels
+    {
+        public class Product
+        {
+
+            [Key]
+            public int pId { get; set; }
+            public string Name { get; set; }
+            public string Course { get; set; }
+            public string Email { get; set; }
+        }
+
+
+        // DbContext used to perform operations on database tables
+        public class ProductDbContext : DbContext
+        {
+            // This property refer the databse table 
+            // Multiple tables required multiple properties
+            public DbSet<Product> Products { get; set; }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Product>()
+                    .HasKey(p => p.pId);
+
+                // Other configurations
+            }
+            public ProductDbContext(DbContextOptions<ProductDbContext> options)
+             : base(options)
+            {
+
+            }
+
+
+        }
+    }
+}
